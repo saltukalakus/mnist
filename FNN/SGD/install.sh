621d95a72fcec33bf3290646d4229522514264c7
Saltuk Alakus <saltukalakus@gmail.com>
2016-07-12 18:18:14 +0300

    feedforward neural net with SDG alg.

---
+++ FNN/SGD/install.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install python-dev python-devel python-pip python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+pip install -r ./requirements.txt
